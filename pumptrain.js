4d8985793dac09fd8d515b9eabb23e16c17d3b29
David Albert <davidbalbert@gmail.com>
2012-11-10 12:58:36 -0500

    Added the train

--- pumptrain.js
+++ pumptrain.js
@@ -1,3 +1,20 @@
-function loaded() {
-  console.log("Hello, PumpTrain!");
+window.onload = function() {
+  WIDTH = 800;
+  HEIGHT = 576;
+
+  Crafty.init(WIDTH, HEIGHT);
+  Crafty.canvas.init();
+
+  Crafty.sprite(32, "sprites.png", {
+    train: [0, 0]
+  });
+
+  Crafty.scene("main", function() {
+    Crafty.e("2D, Canvas, train, Multiway")
+      .attr({x: Crafty.math.randomInt(0, WIDTH - 32),
+             y: Crafty.math.randomInt(0, HEIGHT - 32)})
+      .multiway(4, {UP_ARROW: -90, DOWN_ARROW: 90, RIGHT_ARROW: 0, LEFT_ARROW: 180});
+  });
+
+  Crafty.scene("main");
 }
