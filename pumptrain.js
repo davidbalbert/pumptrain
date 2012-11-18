2b395fc50fbc8de3cbeb734b52bfd8fb46bda94d
Michael P. Geraci <me@mgeraci.com>
2012-11-17 21:34:09 -0500

    move analytics back to index

--- pumptrain.js
+++ pumptrain.js
@@ -40,18 +40,6 @@ window.onload = function() {
   Crafty.init(WIDTH, HEIGHT);
   Crafty.canvas.init();
 
-  // start analytics
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-36407361-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-  // end analytics
-
   flashScreen = document.createElement('div');
   flashScreen.className = 'flash-screen'
   document.getElementById('cr-stage').appendChild(flashScreen);
