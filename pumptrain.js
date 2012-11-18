88a6185f7bea9cb3ae09e2ab37a5aaa84e11d352
Michael P. Geraci <me@mgeraci.com>
2012-11-17 21:00:29 -0500

    move analytics code to main function

--- pumptrain.js
+++ pumptrain.js
@@ -6,18 +6,6 @@ window.onkeydown = function(e) {
   }
 };
 
-var setupAnalytics = function() {
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-36407361-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-};
-
 window.onload = function() {
   var WIDTH = 800;
   var HEIGHT = 576;
@@ -52,7 +40,17 @@ window.onload = function() {
   Crafty.init(WIDTH, HEIGHT);
   Crafty.canvas.init();
 
-  setupAnalytics();
+  // start analytics
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36407361-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+  // end analytics
 
   flashScreen = document.createElement('div');
   flashScreen.className = 'flash-screen'
